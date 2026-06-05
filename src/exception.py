0254df0d33d2877ae0afdfbaf6598638f03883d5
Sanjana Ingleshwar <basveshwaringleshwar024@gmail.com>
2026-06-05 22:25:34 +0530

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,4 @@
 import sys
-import logging
-from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -17,3 +15,13 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message,
+            error_detail=error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
